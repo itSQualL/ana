882668fa8127cd9b196501d9e36ae28b8eedc3cf
Sergio Ruiz <sergio.ruiz@bizneo.com>
2018-06-14 19:36:59 +0200

    Add tests for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  let(:good_attrs) do
+    {
+      name: "John",
+      nickname: "Cena",
+      email: "john@cena.com",
+      password: "asdfasdf"
+    }
+  end
+
+  let(:bad_attrs) do
+    {
+      name: "John",
+      nickname: "Cena",
+      email: "john@cena.com",
+      password: nil
+    }
+  end
+
+  it "should be valid with good attributes" do
+    expect(User.new(good_attrs)).to be_valid
+  end
+
+  it "should be invalid with bad attributes" do
+    expect(User.new(bad_attrs)).to_not be_valid
+  end
+end
