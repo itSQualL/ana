5b5ec1fe296eab930adaf254f4ef6032600f2519
Sergio Ruiz <sergio.ruiz@bizneo.com>
2018-06-14 21:53:33 +0200

    Add to docker bundle cache

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,14 @@ FROM ruby:2.5
 
 MAINTAINER Sergio Ruiz <sergioacaraballo@gmail.com>
 
-WORKDIR /app
-
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
 
-COPY Gemfile /app/Gemfile
-COPY Gemfile.lock /app/Gemfile.lock
+# Set ruby console history
+# Enable console history
+RUN echo "require 'irb/ext/save-history'" | tee -a ~/.irbrc
+RUN echo "IRB.conf[:SAVE_HISTORY] = 2000" | tee -a ~/.irbrc
+RUN echo "IRB.conf[:HISTORY_FILE] = '~/irb_history/.irb-history'" | tee -a ~/.irbrc
+ENV HISTFILE=/root/bash_history/.bash_history
 
-RUN bundle install
+RUN mkdir -p /app
+WORKDIR /app
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,14 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user, nickname: "u2", email: "u2@2.com") }
+
+  let(:good_attrs) { { follower_id: user1.id, followed_id: user2.id } }
+  let(:bad_attrs) { { follower_id: user1.id } }
+  let(:af_attrs) { { follower_id: user1.id, followed_id: user1.id } }
 
   it "should be valid with good attributes" do
+    expect(Relationship.new(good_attrs)).to be_valid
   end
 
   it "should be invalid with bad attributes" do
+    expect(Relationship.new(bad_attrs)).to_not be_valid
   end
 
   it "shouldn't permit autofollow" do
+    expect(Relationship.new(af_attrs)).to_not be_valid
   end
 
 end
